2d7c99b9ead0aba640b7aff3cbfeaebeb25c6266
Mariana Santos <maririb51@gmail.com>
2024-02-14 19:05:22 +0000

    Get user by id

--- src/main/java/com/mariana/workshopmongo/service/UserService.java
+++ src/main/java/com/mariana/workshopmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.mariana.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.mariana.workshopmongo.models.DTO.UserDTO;
 import com.mariana.workshopmongo.models.entities.User;
 import com.mariana.workshopmongo.repositories.UserRepository;
+import com.mariana.workshopmongo.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -19,6 +21,12 @@ public class UserService {
 	public List<UserDTO> findAll(){
 		List<User> list= repository.findAll();
 	 return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	public UserDTO findById(String id) {
+		Optional<User>result = repository.findById(id);
+		User entity = result.orElseThrow(()-> new ResourceNotFoundException("Object not found"));
+		return new UserDTO(entity);
 	}
 	
 
